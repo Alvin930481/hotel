217b637bc30d7f25bc1d4d5b41f2e0efab35fd39
chun <kao.chun@pdcase.com.br>
2024-12-03 11:18:59 +0800

    add security, Jwt and Filter, swagger

---
+++ src/main/java/com/kaolee/hotel/configuration/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.kaolee.hotel.configuration;
+
+import com.kaolee.hotel.configuration.utils.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // 禁用 CSRF（僅在開發環境建議）
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/user/login/**",//登入的路徑
+                                 "/api-docs/**",  // Swagger 的 API 文檔路徑
+                                 "/swagger-ui/**",   // Swagger UI 的靜態資源
+                                 "/swagger-ui.html"  // Swagger UI 主頁
+                        ).permitAll() // 公開路由
+                        .anyRequest().authenticated()             // 其他路由需要驗證
+                )
+                .formLogin(Customizer.withDefaults())          // 啟用預設登入表單
+                .logout(Customizer.withDefaults());            // 啟用預設登出
+//                有前端的時用到
+//                .formLogin(form -> form
+//                        .loginPage("/login")
+//                        .permitAll())          // 啟用預設登入表單
+//                .logout(logout -> logout
+//                        .logoutUrl("/logout")
+//                        .logoutSuccessUrl("/public/home")         // 登出後跳轉的頁面
+//                        .permitAll() );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder authManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authManagerBuilder
+                .userDetailsService(username ->
+                        new org.springframework.security.core.userdetails.User(
+                                username,
+                                passwordEncoder.encode("password"),
+                                new ArrayList<>()
+                        )
+                )
+                .passwordEncoder(passwordEncoder);
+        return authManagerBuilder.build();
+    }
+}
---
+++ src/main/java/com/kaolee/hotel/configuration/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.kaolee.hotel.configuration;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        // Bearer Token 設定
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("Authorization")
+                .in(SecurityScheme.In.HEADER);
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Hotel API Documentation")
+                        .version("1.0.0")
+                        .description("Hotel API with JWT and API Token authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", bearerScheme)) // 加入 Bearer Token 設定
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")); // 將 Bearer 授權應用於全局
+    }
+}
